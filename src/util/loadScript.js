c4b87bec49f786246ae0ebba548d46adb4fe842f
聆一 <fenfen.wff@alibaba-inc.com>
2019-12-26 10:41:55 +0800

    chore: update dist & l7 version

--- src/util/loadScript.js
+++ src/util/loadScript.js
@@ -2,6 +2,7 @@ export default function loadScript (src) {
   return new Promise((resolve, reject) => {
     // 避免重复加载
     if (window.AMap) {
+      console.log('AMap已存在，返回成功');
       resolve()
     }
 
@@ -9,11 +10,13 @@ export default function loadScript (src) {
       var script = document.createElement('script')
       script.src = src
       script.onload = () => {
+        console.log('AMap加载成功');
         setTimeout(resolve, 10)
       }
       script.onerror = reject
       document.head.appendChild(script)
     } catch (err) {
+      console.log('AMap加载失败');
       reject(err)
     }
   })
